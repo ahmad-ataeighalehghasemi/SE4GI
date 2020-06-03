9bab90a78c006b8fb9630b28cd6f83d4e3a6d46e
abdoul-creator <64147006+abdoul-creator@users.noreply.github.com>
2020-06-03 06:23:40 +0200

    Add files via upload

--- tree_inventory/data/graphs_functions.py
+++ tree_inventory/data/graphs_functions.py
@@ -6,4 +6,25 @@ Created on Tue Jun  2 17:52:08 2020
 """
 
 from raw_data import *
-print(df)
+import geopandas as gpd
+from shapely.geometry import Point, LineString, Polygon
+import fiona
+import numpy as np
+import matplotlib.pyplot as plt
+import contextily as ctx
+df=gpd.GeoDataFrame(df) #creat the geodatabase
+#plot of the position 
+df['geometry']=None
+for i in range(len(df)):
+    latitude=df['3_GPS_location_taken'][i]['latitude']
+    longitude=df['3_GPS_location_taken'][i]['longitude']
+    if latitude or longitude !="":
+        df['geometry'][i]=Point(longitude,latitude)
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+df.crs = fiona.crs.from_epsg(4326) # Set the coordinate system to WGS84 using Fiona module
+df.to_file(r'position.shp')
+position = gpd.read_file(r'position.shp')
+ax = world[world.continent == 'North America'].plot(color='white', edgecolor='black')
+position.plot(ax=ax, color='red')
+
