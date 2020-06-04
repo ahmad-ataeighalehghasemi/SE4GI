d6738feb9a3dc6155ae0bc36b934d5aaf8190279
abdoul-creator <64147006+abdoul-creator@users.noreply.github.com>
2020-06-04 03:57:05 +0200

    Add files via upload

--- tree_inventory/data/graphs_functions.py
+++ tree_inventory/data/graphs_functions.py
@@ -12,6 +12,19 @@ import fiona
 import numpy as np
 import matplotlib.pyplot as plt
 import contextily as ctx
+from bokeh.plotting import figure, show, output_file
+
+import pandas as pd
+#Importing the required packages
+import geopandas as gpd
+import pandas as pd
+from bokeh.models import ColumnDataSource, LabelSet, Select
+from bokeh.plotting import figure, show, output_file
+#from bokeh.tile_providers import get_provider, Vendors #bokeh version 1.1
+from bokeh.tile_providers import CARTODBPOSITRON #bokeh version 1.0
+from bokeh.io import curdoc
+from bokeh.layouts import row
+
 df=gpd.GeoDataFrame(df) #creat the geodatabase
 #plot of the position 
 df['geometry']=None
@@ -22,9 +35,104 @@ for i in range(len(df)):
         df['geometry'][i]=Point(longitude,latitude)
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 #cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
-df.crs = fiona.crs.from_epsg(4326) # Set the coordinate system to WGS84 using Fiona module
+df.crs = fiona.crs.from_epsg(3857) # Set the coordinate system to WGS84 using Fiona module
 df.to_file(r'position.shp')
 position = gpd.read_file(r'position.shp')
+
+#mapview geodateframe
 ax = world[world.continent == 'North America'].plot(color='white', edgecolor='black')
 position.plot(ax=ax, color='red')
 
+
+
+
+
+
+#Map_vue
+##ax = world[world.continent == 'North America'].plot(color='white', edgecolor='black')
+##position.plot(ax=ax, color='red')
+#position_df = position.drop('geometry', axis=1).copy()
+#psource = ColumnDataSource(position_df)
+#p1 = figure(x_range=(-9780000, -9745000), y_range=(5130000, 5160000),
+#           x_axis_type="mercator", y_axis_type="mercator")
+#p1.add_tile(get_provider(Vendors.CARTODBPOSITRON))
+#p1.circle('x', 'y', source=psource, color='red', radius=10)
+#output_file("tree_map.html")
+#show(p1)
+
+#barplot of circumference
+nb_tree = list(df.index)
+cir=list(df['4_Circumference_of_t'])
+data = ColumnDataSource({'x' : nb_tree, 'y': cir})
+p2 = figure()
+p2.vbar(x='x', top='y', source = data, width=0.9) 
+show(p2)
+output_file('barplot.html')
+
+#Barplot of the type of tree
+
+
+
+
+
+
+
+
+
+
+
+
+
+#histogramme 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#cartogramme
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
